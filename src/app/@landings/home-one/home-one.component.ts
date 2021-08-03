bba42044ffe875b2fe438cfa848ffac99e45350e
George Petrakis <georgepetrakis99@gmail.com>
2021-08-03 13:14:08 +0300

    Work on filters selection

--- src/app/@landings/home-one/home-one.component.ts
+++ src/app/@landings/home-one/home-one.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Content_model } from 'src/app/@Models/Content_model';
 import { ParserService } from 'src/app/@Services/parser.service';
-import { IMultiSelectOption } from 'ngx-bootstrap-multiselect';
+import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'ngx-bootstrap-multiselect';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-home-one',
@@ -14,18 +15,52 @@ export class HomeOneComponent implements OnInit {
 
   gridColumns = 6;
 
-  optionsModel: number[];
-  myOptions: IMultiSelectOption[];
+  optionsModel: number[] = [1,2,3,4,5,6,7,8];
+
+  // Settings configuration
+  mySettings: IMultiSelectSettings = {
+    enableSearch: false,
+    showCheckAll: true,
+    fixedTitle: true,
+    showUncheckAll: true,
+    checkedStyle: 'checkboxes',
+    buttonClasses: 'btn btn-default btn-block',
+    dynamicTitleMaxItems: 0,
+    displayAllSelectedText: false
+  };
+
+  //options config
+  myOptions: IMultiSelectOption[] = [
+    { id: 1, name: 'Car brands', isLabel: true },
+    { id: 2, name: 'Volvo', parentId: 1 },
+    { id: 3, name: 'Honda', parentId: 1 },
+    { id: 4, name: 'BMW', parentId: 1 },
+    { id: 5, name: 'Colors', isLabel: true },
+    { id: 6, name: 'Blue', parentId: 5 },
+    { id: 7, name: 'Red', parentId: 5 },
+    { id: 8, name: 'White', parentId: 5 }
+];
+
+// Text configuration
+myTexts: IMultiSelectTexts = {
+  checkAll: 'Check all',
+  uncheckAll: 'Uncheck all',
+  checked: 'item selected',
+  checkedPlural: 'items selected',
+  searchPlaceholder: 'Find',
+  searchEmptyResult: 'Nothing found...',
+  searchNoRenderText: 'Type in search box to see results...',
+  defaultTitle: 'Filters',
+  allSelected: 'All selected',
+};
 
   constructor(private parser: ParserService) { }
 
   ngOnInit(): void {
     this.content = this.parser.parse();
 
-    this.myOptions = [
-      { id: 1, name: 'Option 1' },
-      { id: 2, name: 'Option 2' },
-    ];
+    
+
   }
   onChange() {
     console.log(this.optionsModel);
