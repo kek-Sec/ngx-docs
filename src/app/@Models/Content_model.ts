afdacce93272dbcb37545df00583a0278fc6b664
George Petrakis <georgepetrakis99@gmail.com>
2021-08-03 11:11:30 +0300

    Added models and services

---
+++ src/app/@Models/Content_model.ts
@@ -0,0 +1,6 @@
+export class Content_model{
+    Title:string;
+    Description:string;
+    text:string;
+    image:string;
+}
---
+++ src/app/@Models/Documentation_model.ts
@@ -0,0 +1,6 @@
+import { Content_model } from "./Content_model";
+
+export class Documentation_model{
+    date_created: string;
+    content: Content_model[];
+  }
