420320f0be3145bf038dc21a3d6ef97e325ce4d3
George Petrakis <georgepetrakis99@gmail.com>
2021-08-03 12:25:12 +0300

    Added ngx-bootstrap dropdown

--- src/app/@landings/home-one/home-one.component.ts
+++ src/app/@landings/home-one/home-one.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Content_model } from 'src/app/@Models/Content_model';
 import { ParserService } from 'src/app/@Services/parser.service';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-home-one',
   templateUrl: './home-one.component.html',
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }],
   styleUrls: ['./home-one.component.scss']
 })
 export class HomeOneComponent implements OnInit {
@@ -13,7 +15,7 @@ export class HomeOneComponent implements OnInit {
   gridColumns = 6;
 
 
-  constructor(private parser:ParserService) { }
+  constructor(private parser: ParserService) { }
 
   ngOnInit(): void {
     this.content = this.parser.parse();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeOneComponent } from './@landings/home-one/home-one.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+//ngx-bootstrap
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeOneComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
     FlexLayoutModule,
     BrowserModule,
     AppRoutingModule
