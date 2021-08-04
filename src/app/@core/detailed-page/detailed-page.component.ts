9e4290527ce51f26987c4e9399ad41d1470792e1
George Petrakis <georgepetrakis99@gmail.com>
2021-08-04 10:02:58 +0300

    work on detailed-page .ts

--- src/app/@core/detailed-page/detailed-page.component.ts
+++ src/app/@core/detailed-page/detailed-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailed-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailedPageComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  private sub: any;
+
+
+  constructor(private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
   }
 
 }
