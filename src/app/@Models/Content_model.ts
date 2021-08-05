1001abfed2c5f76861051d63e2a83df0100df568
kek-sec <georgepetrakis99@gmail.com>
2021-08-05 22:06:11 +0300

    Some work on content model

--- src/app/@Models/Content_model.ts
+++ src/app/@Models/Content_model.ts
@@ -1,8 +1,16 @@
 export class Content_model {
+
     Title: string;
     Description: string;
     text: string;
     image: string;
     items: string[];
     tags?: string[];
+    url?: string;
+    album?: string[];
+
+    //properties
+
+    show_picture?: boolean = true;
+    
 }
