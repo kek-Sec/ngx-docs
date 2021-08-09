7f3a38410476cf4e81f313702788e2a05b00aef8
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 16:53:07 +0300

    Add entry implemented

--- .@Services/Input_Service.cs
+++ .@Services/Input_Service.cs
@@ -1,7 +1,9 @@
 ﻿using ngx_docs_managment_application._Forms;
 using ngx_docs_managment_application._Models;
+using ngx_docs_managment_application._Serializers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ namespace ngx_docs_managment_application._Controllers
     class Input_Service
     {
         readonly Input_Parser parser = new Input_Parser();
+        readonly Input_Serializer serializer = new Input_Serializer();
         readonly Settings_Service settings = new Settings_Service();
 
         public IList<Input_Model> input_collection;
@@ -24,6 +27,9 @@ namespace ngx_docs_managment_application._Controllers
         public void AddEntry(Input_Model input)
         {
             input_collection.Add(input);
+            var filepath = settings.getSetting("project_path") + "\\src\\input2.json";
+            var json = serializer.Serialize(input_collection);
+            File.WriteAllText(filepath, json);
 
         }
 
