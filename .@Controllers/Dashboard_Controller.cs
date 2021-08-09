fb5b2bc536422b530a50f35ba5d480a5eb414e50
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 13:39:11 +0300

    work on dashboard controller

--- .@Controllers/Dashboard_Controller.cs
+++ .@Controllers/Dashboard_Controller.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ngx_docs_managment_application._Controllers
 {
     class Dashboard_Controller
     {
-        
+        Dashboard_Service ds = new Dashboard_Service();
+
+        /// <summary>
+        /// Updates the label that states the status of the project folder directory
+        /// </summary>
+        /// <param name="status">The status label</param>
+        public void UpdateProjectFolderLabel(Label status)
+        {
+            bool ok = ds.VerifyProjectFolder();
+            if(ok)
+            {
+                status.ForeColor = Color.LightSeaGreen;
+                status.Text = "Verified!";
+                return;
+            }
+
+            //not ok
+            status.ForeColor = Color.MediumVioletRed;
+            status.Text = "Not Verified!";
+            return;
+        }
+
     }
 }
