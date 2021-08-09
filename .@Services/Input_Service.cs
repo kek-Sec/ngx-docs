060754c4e50be1dede82d8f2c3a8171339cf9dae
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 14:05:04 +0300

    work on input Service

--- .@Services/Input_Service.cs
+++ .@Services/Input_Service.cs
@@ -1,4 +1,5 @@
 ﻿using ngx_docs_managment_application._Forms;
+using ngx_docs_managment_application._Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,22 @@ namespace ngx_docs_managment_application._Controllers
     class Input_Service
     {
         readonly Input_Parser parser = new Input_Parser();
+        readonly Settings_Service settings = new Settings_Service();
 
+        public IList<Input_Model> input_collection;
+
+        Input_Service()
+        {
+            try
+            {
+                var filepath = settings.getSetting("project_path") + "\\src\\input.json";
+                input_collection = parser.parse(filepath);
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
 
     }
 }
