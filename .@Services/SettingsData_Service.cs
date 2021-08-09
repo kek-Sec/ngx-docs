5259a22c81afaf65755b75075f55478915e17c03
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 15:08:58 +0300

    Setting data service

---
+++ .@Services/SettingsData_Service.cs
@@ -0,0 +1,28 @@
+﻿using ngx_docs_managment_application._Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngx_docs_managment_application._Forms
+{
+    class SettingsData_Service
+    {
+        Settings_Service settings = new Settings_Service();
+        Settings_Parser parser = new Settings_Parser();
+        Settings_Model settings_model = new Settings_Model();
+        public SettingsData_Service()
+        {
+            try
+            {
+                var filepath = settings.getSetting("project_path") + "\\src\\input.json";
+                settings_model = parser.parse(filepath);
+            }
+            catch (Exception e)
+            {
+                Console.Write("error -> " + e.Message.ToString());
+            }
+        }
+    }
+}
