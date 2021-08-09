07f67adf527c09a16c7ec4082eeca43c9b91e837
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 13:46:36 +0300

    input service

---
+++ .@Services/Input_Service.cs
@@ -0,0 +1,16 @@
+﻿using ngx_docs_managment_application._Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngx_docs_managment_application._Controllers
+{
+    class Input_Service
+    {
+        readonly Input_Parser parser = new Input_Parser();
+
+
+    }
+}
