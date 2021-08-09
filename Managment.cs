4c5cd02f8c177a0dfae77058c222b9c5d6c39dc1
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 12:19:46 +0300

    modelling

---
+++ Managment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ngx_docs_managment_application
+{
+    public partial class Managment : Form
+    {
+        public Managment()
+        {
+            InitializeComponent();
+        }
+    }
+}
