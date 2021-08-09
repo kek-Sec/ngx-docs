b0fe0af082e414a529123d9a817fc25c405a3918
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 13:12:53 +0300

    work on input parser

--- .@Parsers/Input_Parser.cs
+++ .@Parsers/Input_Parser.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ngx_docs_managment_application._Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,16 @@ namespace ngx_docs_managment_application._Forms
 {
     class Input_Parser
     {
+
+        /// <summary>
+        /// Parse given json and map it to an input model
+        /// </summary>
+        /// <param name="filename">The file to parse</param>
+        /// <returns>Input model object</returns>
+        public Input_Model parse(string filename)
+        {
+            string json = File.ReadAllText(filename);
+            return JsonConvert.DeserializeObject<Input_Model>(json);
+        }
     }
 }
