800aa5fbe35e2b3ee374429dfc67238990fc93d2
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 13:43:16 +0300

    settings controller work

--- .@Controllers/Settings_Controller.cs
+++ .@Controllers/Settings_Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ngx_docs_managment_application._Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace ngx_docs_managment_application._Controllers
 {
     class Settings_Controller
     {
+        readonly Settings_Service settings = new Settings_Service();
+
+        /// <summary>
+        /// Middleware function to start the folder selection routine
+        /// </summary>
+        public void SelectProjectPath()
+        {
+            settings.LoadKeysFolder();
+        }
     }
 }
