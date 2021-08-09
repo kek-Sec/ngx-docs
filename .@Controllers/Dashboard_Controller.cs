1b25a1c84f97109ca329aa49769b3e3505a2591c
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 13:14:18 +0300

    Added controllers

---
+++ .@Controllers/Dashboard_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngx_docs_managment_application._Controllers
+{
+    class Dashboard_Controller
+    {
+    }
+}
