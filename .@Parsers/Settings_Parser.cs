b611f116be587fa38f97ad4048fe5cf537987aa0
George Petrakis <georgepetrakis99@gmail.com>
2021-08-09 15:05:36 +0300

    work on settings parser

--- .@Parsers/Settings_Parser.cs
+++ .@Parsers/Settings_Parser.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ngx_docs_managment_application._Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,11 @@ namespace ngx_docs_managment_application._Forms
 {
     class Settings_Parser
     {
+        public Settings_Model parse(string filename)
+        {
+            string json = File.ReadAllText(filename);
+            Settings_Model ret = JsonConvert.DeserializeObject<Settings_Model>(json);
+            return ret;
+        }
     }
 }
