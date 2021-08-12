94cf66a61a9ee5d5e29ddd119d5f3bc60f475cfa
George Petrakis <georgepetrakis99@gmail.com>
2021-08-12 16:09:50 +0300

    Added some controllers and services

--- .@Services/Images_Service.cs
+++ .@Services/Images_Service.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ngx_docs_managment_application._Controllers
+namespace ngx_docs_managment_application._Services
 {
-    class Theming_Controller
+    class Images_Service
     {
     }
 }
