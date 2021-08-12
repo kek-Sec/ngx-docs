93c133e8d7771af91dbc62ce94658bcb6bb13014
George Petrakis <georgepetrakis99@gmail.com>
2021-08-12 16:46:56 +0300

    Added logger service

---
+++ .@Services/Logger_Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ngx_docs_managment_application._Services
+{
+    class Logger_Service
+    {
+        public void Add(string title,string content)
+        {
+            FormattableString log_entry = $"[{title}]-[{DateTime.Now.ToString("MM-dd-HH-mm")}]----> {content}\n";
+            File.AppendAllText("log.txt", log_entry.ToString());
+        }
+    }
+}
