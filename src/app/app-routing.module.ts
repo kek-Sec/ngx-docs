53135ae21fdba3491e3b4986fd60b484b914c533
George Petrakis <georgepetrakis99@gmail.com>
2021-08-12 16:57:11 +0300

    small styling fix on home two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeOneComponent } from './@landings/home-one/home-one.component';
 import { HomeTwoComponent } from './@landings/home-two/home-two.component';
 
 const routes: Routes = [
-  { path: '', component: HomeOneComponent },
+  { path: '', component: HomeTwoComponent },
   { path: 'view/undefined', redirectTo: '', pathMatch: 'full' },
   { path: 'view/:id', component: DetailedPageTwoComponent },
   { path: 'album/undefined', redirectTo: '', pathMatch: 'full' },
