4eb1b97afb7d9d76ed0ee18865c382a49497b9c4
George Petrakis <georgepetrakis99@gmail.com>
2021-08-13 12:30:57 +0300

    detailed page three styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlbumPageComponent } from './@core/album-page/album-page.component';
 import { DetailedPageComponent } from './@core/detailed-page-one/detailed-page.component';
+import { DetailedPageThreeComponent } from './@core/detailed-page-three/detailed-page-three.component';
 import { DetailedPageTwoComponent } from './@core/detailed-page-two/detailed-page-two.component';
 import { HomeOneComponent } from './@landings/home-one/home-one.component';
 import { HomeTwoComponent } from './@landings/home-two/home-two.component';
@@ -9,7 +10,7 @@ import { HomeTwoComponent } from './@landings/home-two/home-two.component';
 const routes: Routes = [
   { path: '', component: HomeTwoComponent },
   { path: 'view/undefined', redirectTo: '', pathMatch: 'full' },
-  { path: 'view/:id', component: DetailedPageTwoComponent },
+  { path: 'view/:id', component: DetailedPageThreeComponent },
   { path: 'album/undefined', redirectTo: '', pathMatch: 'full' },
   { path: 'album/:id', component: AlbumPageComponent },
 
