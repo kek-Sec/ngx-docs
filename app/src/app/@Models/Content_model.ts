d6345a5e61548b9e7ee1b812fe46d2d5532b146d
kek-Sec <georgepetrakis99@gmail.com>
2021-08-25 14:06:57 +0300

    Update Content_model.ts
    making more fields optional

--- app/src/app/@Models/Content_model.ts
+++ app/src/app/@Models/Content_model.ts
@@ -2,10 +2,10 @@
 export class Content_model {
   id: Number;
   Title: string;
-  Description: string;
-  text: string;
-  image: string;
-  items: string[];
+  Description?: string;
+  text?: string;
+  image?: string;
+  items?: string[];
   tags?: string[];
   url?: string;
   album?: string[];
