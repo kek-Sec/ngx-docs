9f91c24a8a44909ceb97df6dcc1ca8d5ee36ada6
kek-Sec <georgepetrakis99@gmail.com>
2021-08-29 16:34:18 +0300

    SettingsDataController renaming of variables

--- management-app-c#/.@Controllers/SettingsData_Controller.cs
+++ management-app-c#/.@Controllers/SettingsData_Controller.cs
@@ -25,23 +25,23 @@ namespace ngx_docs_managment_application._Forms
         /// <param name="item_length"></param>
         /// <param name="items_count"></param>
         /// <param name="column_count"></param>
-        public void UpdateSettingsDataView(CheckBox show_code,CheckBox show_picture, CheckBox show_title, CheckBox show_description, CheckBox show_text, CheckBox show_items, CheckBox show_url, CheckBox show_album, CheckBox show_tags, NumericUpDown description_length, NumericUpDown text_length, NumericUpDown item_length, NumericUpDown items_count, NumericUpDown column_count)
+        public void UpdateSettingsDataView(CheckBox preview_show_code, CheckBox preview_show_picture, CheckBox preview_show_title, CheckBox preview_show_description, CheckBox preview_show_text, CheckBox preview_show_items, CheckBox preview_show_url, CheckBox preview_show_album, CheckBox preview_show_tags, NumericUpDown description_length, NumericUpDown text_length, NumericUpDown item_length, NumericUpDown items_count, NumericUpDown column_count)
         {
-            try
+            try 
             {
                 if(ss.settings_model is null) { ss.RefreshData(); return; }
 
                 var setting = ss.settings_model[0];
 
-                show_code.Checked = setting.preview_show_code;
-                show_picture.Checked = setting.preview_show_picture;
-                show_title.Checked = setting.preview_show_title;
-                show_description.Checked = setting.preview_show_description;
-                show_text.Checked = setting.preview_show_text;
-                show_items.Checked = setting.preview_show_items;
-                show_url.Checked = setting.preview_show_url;
-                show_tags.Checked = setting.preview_show_tags;
-                show_album.Checked = setting.preview_show_album;
+                preview_show_code.Checked = setting.preview_show_code;
+                preview_show_picture.Checked = setting.preview_show_picture;
+                preview_show_title.Checked = setting.preview_show_title;
+                preview_show_description.Checked = setting.preview_show_description;
+                preview_show_text.Checked = setting.preview_show_text;
+                preview_show_items.Checked = setting.preview_show_items;
+                preview_show_url.Checked = setting.preview_show_url;
+                preview_show_tags.Checked = setting.preview_show_tags;
+                preview_show_album.Checked = setting.preview_show_album;
 
                 text_length.Value = setting.preview_text_length;
                 item_length.Value = setting.preview_item_text_length;
@@ -72,20 +72,20 @@ namespace ngx_docs_managment_application._Forms
         /// <param name="item_length"></param>
         /// <param name="items_count"></param>
         /// <param name="column_count"></param>
-        public void SaveSettings(CheckBox show_code,CheckBox show_picture, CheckBox show_title, CheckBox show_description, CheckBox show_text, CheckBox show_items, CheckBox show_url, CheckBox show_album, CheckBox show_tags, NumericUpDown description_length, NumericUpDown text_length, NumericUpDown item_length, NumericUpDown items_count, NumericUpDown column_count)
+        public void SaveSettings(CheckBox preview_show_code, CheckBox preview_show_picture, CheckBox preview_show_title, CheckBox preview_show_description, CheckBox preview_show_text, CheckBox preview_show_items, CheckBox preview_show_url, CheckBox preview_show_album, CheckBox preview_show_tags, NumericUpDown description_length, NumericUpDown text_length, NumericUpDown item_length, NumericUpDown items_count, NumericUpDown column_count)
         {
             try
             {
                 Settings_Model settings = new Settings_Model();
-                settings.preview_show_code = show_code.Checked;
-                settings.preview_show_picture = show_picture.Checked;
-                settings.preview_show_title = show_title.Checked;
-                settings.preview_show_description = show_description.Checked;
-                settings.preview_show_text = show_text.Checked;
-                settings.preview_show_items = show_items.Checked;
-                settings.preview_show_url = show_url.Checked;
-                settings.preview_show_album = show_album.Checked;
-                settings.preview_show_tags = show_tags.Checked;
+                settings.preview_show_code = preview_show_code.Checked;
+                settings.preview_show_picture = preview_show_picture.Checked;
+                settings.preview_show_title = preview_show_title.Checked;
+                settings.preview_show_description = preview_show_description.Checked;
+                settings.preview_show_text = preview_show_text.Checked;
+                settings.preview_show_items = preview_show_items.Checked;
+                settings.preview_show_url = preview_show_url.Checked;
+                settings.preview_show_album = preview_show_album.Checked;
+                settings.preview_show_tags = preview_show_tags.Checked;
                 settings.preview_description_text_length = (int)description_length.Value;
                 settings.preview_item_text_length = (int)item_length.Value;
                 settings.preview_items_count = (int)items_count.Value;
