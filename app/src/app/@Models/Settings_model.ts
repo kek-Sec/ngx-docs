635823ddc28089e9716f5fe41df626655a325090
kek-Sec <georgepetrakis99@gmail.com>
2021-08-29 16:24:49 +0300

    Starting settings rework
    Will need to add detailed page settings

--- app/src/app/@Models/Settings_model.ts
+++ app/src/app/@Models/Settings_model.ts
@@ -1,4 +1,6 @@
 export class Settings_model {
+
+  //preview settings
   preview_show_picture?: boolean = true;
   preview_show_title?: boolean = true;
   preview_show_description?: boolean = true;
@@ -7,12 +9,21 @@ export class Settings_model {
   preview_show_url?: boolean = false;   //not implemented
   preview_show_album?: boolean = false; //not implemented
   preview_show_tags?: boolean = false;  //not implemented
-
-  //preview settings
   preview_description_text_length?: number = 15;
   preview_text_length?: number = 15;
   preview_item_text_length?: number = 4;
   preview_items_count?: number = 3;
+
+  //detailed-page settings
+  detailed_show_picture?: boolean = true;
+  detailed_show_title?: boolean = true;
+  detailed_show_description?: boolean = true;
+  detailed_show_text?: boolean = true;
+  detailed_show_items?: boolean = true;
+  detailed_show_url?: boolean = false;  
+  detailed_show_album?: boolean = false;
+  detailed_show_tags?: boolean = false; 
+
 
   //flex layout settings
   preview_number_of_columns?: number = 4;
