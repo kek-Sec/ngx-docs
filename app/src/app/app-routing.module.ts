234866c331346e3ff79f576402e339f173e283b5
kek-Sec <georgepetrakis99@gmail.com>
2021-09-02 11:17:22 +0300

    detailed pages work

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AlbumPageComponent } from './@core/album-page/album-page.component';
 import { DetailedPageOneComponent } from './@core/detailed-page-one/detailed-page-one.component';
 import { DetailedPageThreeComponent } from './@core/detailed-page-three/detailed-page-three.component';
+import { DetailedPageTwoComponent } from './@core/detailed-page-two/detailed-page-two.component';
 import { HomeOneComponent } from './@landings/home-one/home-one.component';
 import { HomeTwoComponent } from './@landings/home-two/home-two.component';
 
 const routes: Routes = [
   { path: '', component: HomeTwoComponent },
   { path: 'view/undefined', redirectTo: '', pathMatch: 'full' },
-  { path: 'view/:id', component: DetailedPageThreeComponent },
+  { path: 'view/:id', component: DetailedPageTwoComponent },
   { path: 'album/undefined', redirectTo: '', pathMatch: 'full' },
   { path: 'album/:id', component: AlbumPageComponent },
 
