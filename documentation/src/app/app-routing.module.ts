76fdf7a225a50521689ab7a9a0defa3f2843b0ee
kek-Sec <georgepetrakis99@gmail.com>
2021-09-03 14:12:52 +0300

    Update app-routing.module.ts

--- documentation/src/app/app-routing.module.ts
+++ documentation/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlbumPageComponent } from './@core/album-page/album-page.component';
 import { DetailedPageOneComponent } from './@core/detailed-page-one/detailed-page-one.component';
-import { DetailedPageThreeComponent } from './@core/detailed-page-three/detailed-page-three.component';
-import { DetailedPageTwoComponent } from './@core/detailed-page-two/detailed-page-two.component';
 import { HomeTwoComponent } from './@landings/home-two/home-two.component';
 
 const routes: Routes = [
